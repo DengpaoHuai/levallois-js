73dbb496e29ca3eda28332e6660e75663aebdde2
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-09-23 11:03:16 +0200

    add planets

--- async.js
+++ async.js
@@ -2,10 +2,32 @@ const getData = async () => {
   try {
     const response = await fetch("https://swapi.dev/api/planets");
     const data = await response.json();
+    const container = document.querySelector("#planets");
+    data.results.forEach((planet) => {
+      console.log(planet.name);
+      const p = document.createElement("p");
+      p.innerText = planet.name;
+      container.appendChild(p);
+    });
   } catch (error) {}
 
   console.log(data);
 };
+
+/*
+const getData1 = () => {
+    return new Promise((resolve, reject) => {
+        fetch("https://swapi.dev/api/planets")
+        .then((response) => {
+            resolve(response.json());
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    })
+}
+
+*/
 
 //getData();
 
--- planets.js
+++ planets.js
@@ -2,7 +2,13 @@ const getData = async () => {
   try {
     const response = await fetch("https://swapi.dev/api/planets");
     const data = await response.json();
-    console.log(data);
+    const container = document.querySelector("#planets");
+    data.results.forEach((planet) => {
+      console.log(planet.name);
+      const p = document.createElement("p");
+      p.innerText = planet.name;
+      container.appendChild(p);
+    });
   } catch (error) {}
 };
 
