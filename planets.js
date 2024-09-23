004fd0dc724786f035f45931094daca607f2f388
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-09-23 11:45:13 +0200

    add pagination

--- planets.js
+++ planets.js
@@ -1,8 +1,32 @@
-const getData = async () => {
+let planetResponse = {
+  count: 0,
+  next: null,
+  previous: null,
+  results: [],
+};
+
+const nextButton = document.getElementById("nextButton");
+const previousButton = document.getElementById("backButton");
+const loader = document.getElementById("loader");
+
+const container = document.querySelector("#planets");
+
+const getData = async (url) => {
+  loader.style.display = "block";
+  nextButton.disabled = true;
+  previousButton.disabled = true;
+  container.innerHTML = "";
   try {
-    const response = await fetch("https://swapi.dev/api/planets");
+    const response = await fetch(url);
     const data = await response.json();
-    const container = document.querySelector("#planets");
+    loader.style.display = "none";
+    planetResponse = data;
+    if (data.next) {
+      nextButton.disabled = false;
+    }
+    if (data.previous) {
+      previousButton.disabled = false;
+    }
     data.results.forEach((planet) => {
       console.log(planet.name);
       const p = document.createElement("p");
@@ -12,4 +36,21 @@ const getData = async () => {
   } catch (error) {}
 };
 
-getData();
+getData("https://swapi.dev/api/planets");
+
+const previousPage = () => {
+  if (planetResponse.previous) {
+    getData(planetResponse.previous);
+  }
+};
+
+nextButton.addEventListener("click", () => {
+  if (planetResponse.next) {
+    getData(planetResponse.next);
+  }
+});
+
+document.getElementById("searchForm").addEventListener("submit", (e) => {
+  console.log(e);
+  e.preventDefault();
+});
